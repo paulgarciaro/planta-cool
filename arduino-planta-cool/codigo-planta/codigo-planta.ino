bf486b8c2e80dbdbf66cea0a76f43ea79f0d7245
Javier Sanchez Panduro <69559745+javiersanpan@users.noreply.github.com>
2021-12-02 10:53:25 -0600

    bomba de agua fixed

--- arduino-planta-cool/codigo-planta/codigo-planta.ino
+++ arduino-planta-cool/codigo-planta/codigo-planta.ino
@@ -15,9 +15,13 @@ FirebaseData firebaseData;
 const int dry = 696;
 const int wet = 336;
 
+// Variables para la bomba de agua
+bool regar = true;
+
 void setup() {
-  // Pines
+  // Iniciar pin de bomba de agua
   pinMode(D0,OUTPUT);
+  digitalWrite(D0, HIGH);
 
   // Monitor serial
   Serial.begin(9600);
@@ -58,11 +62,15 @@ void loop() {
   Firebase.pushInt(firebaseData, "Planta1/humedad-de-la-tierra-historia", percentageMoisture);
   Firebase.setInt(firebaseData, "Planta1/humedad-de-la-tierra-actual", percentageMoisture);
 
-  
+  while(regar){
   // Loop de la bomba de agua
   digitalWrite(D0, LOW);
   delay(1000);
   digitalWrite(D0, HIGH);
+  regar = false;
+  
+  }
+
   delay(3000);
 
 }
